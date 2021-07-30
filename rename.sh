cdb71ec6f5637dc940e863ead46e0df3bfd95391
hakbyte <hakbyte@users.noreply.github.com>
2021-07-30 14:30:51 +0200

    Add shell script to rename images

---
+++ rename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Rename image file. New name is the 20 first characters fo the file's SHA-1 sum
+# plus the image pixel size (e.g. 1920x1080). File extension is preserved
+# FIXME: implement error checking
+function rename {
+    local filename=$1
+    local dimensions=$(identify -format '%wx%h' $filename 2>/dev/null)
+    local sha1sum=$(sha1sum $filename | cut -c 1-20)
+    local new_filename=$(dirname $filename)/${sha1sum}_${dimensions}.${filename##*.}
+
+    echo "Renamed '$filename' to '$new_filename'"
+    mv $filename $new_filename 2>/dev/null
+}
+
+rename $1
