563707cde948d5d1176ec3f69342450d69deed41
hakbyte <hakbyte@users.noreply.github.com>
2021-08-10 16:38:14 +0200

    Handle filenames that contain spaces

--- rename.sh
+++ rename.sh
@@ -4,13 +4,13 @@
 # plus the image pixel size (e.g. 1920x1080). File extension is preserved
 # FIXME: implement error checking
 function rename {
-    local filename=$1
-    local dimensions=$(identify -format '%wx%h' $filename 2>/dev/null)
-    local sha1sum=$(sha1sum $filename | cut -c 1-20)
-    local new_filename=$(dirname $filename)/${sha1sum}_${dimensions}.${filename##*.}
+    local filename=$*
+    local dimensions=$(identify -format '%wx%h' "$filename" 2>/dev/null)
+    local sha1sum=$(sha1sum "$filename" | cut -c 1-20)
+    local new_filename=$(dirname "$filename")/${sha1sum}_${dimensions}.${filename##*.}
 
     echo "Renamed '$filename' to '$new_filename'"
-    mv $filename $new_filename 2>/dev/null
+    mv "$filename" $new_filename 2>/dev/null
 }
 
 rename $1
